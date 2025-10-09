92f16efb2a5676dccf1477cdd613e973ddfcbc1b
Nastia Bzova <bzovanastia@gmail.com>
2025-10-09 16:55:37 +0200

    added links to social medias + added logo-container

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,13 +8,19 @@ const Footer = () => {
       <div className="container">
         <div className="footer-content">
           <div className="footer-section">
-            <h5>Masyoro Vocational School</h5>
-            <p>Empowering students with language skills for global success since 1995. Join our community of learners and unlock your potential.</p>
-            <div className="social-links">
-              <a href="#"><i><FaInstagram/></i></a>
-              <a href="#"><i><FaFacebook/></i></a>
-              <a href="#"><i><FaYoutube /></i></a>
-              <a href="#"><i><FaLinkedin/></i></a>
+            <div>
+              <h5>King's College</h5>
+              <p>Rooted in Belizean pride on the Old Northern Highway since 1967, <br />
+                King's College is dedicated to cultivating well-rounded, ethical leaders. <br />
+                Join our supportive community and achieve excellence.</p>
+              <div className="social-links">
+                <a href="https://www.facebook.com/kings.belize/"><i><FaFacebook/></i></a>
+                <a href="https://www.youtube.com/@kingscollegebelize2093"><i><FaYoutube /></i></a>
+              </div>
+            </div>
+
+            <div className='logo-container'>
+              <img className='logo-fotter' src="/logo.jpg" alt="" />
             </div>
           </div>
         </div>
