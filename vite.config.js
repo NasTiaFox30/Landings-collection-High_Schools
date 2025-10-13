3d165938ed343393983d233538189d0a7953338b
Nastia Bzova <bzovanastia@gmail.com>
2025-10-13 13:14:30 +0200

    update dir of page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: /Kings-College-landing-page/
+  base: /Kings_College-landing-page/
 })
