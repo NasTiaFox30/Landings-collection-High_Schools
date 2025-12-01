9d28826002b68fa07a4b67dffb66a5a87cca07a3
Anastasiia Bzova <bzovanastia@gmail.com>
2025-12-01 01:39:36 +0100

    Updatetd names & homepage in packages json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: /Our_Lady_of_Guadalupe_RC_High_School-landing-page/           //Replace
+  base: /Mount_Carmel_High_School-landing-page/           //Replace
 })
