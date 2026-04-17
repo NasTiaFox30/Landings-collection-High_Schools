fff2557c4110ef636cee4fe9e50e0f8ffbeebc09
Anastasiia Bzova <bzovanastia@gmail.com>
2026-04-17 22:23:00 +0200

    Fix: update base path for Kings_College

--- Kings_College/vite.config.js
+++ Kings_College/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: /Kings_College-landing-page/
+  base: '/Landings-collection-High_Schools/Kings_College/'
 })
