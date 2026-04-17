99539a2af7e13ee9315872c1bd1b5b8ea88e3d13
Anastasiia Bzova <bzovanastia@gmail.com>
2026-04-17 23:38:20 +0200

    Fix: update base path for all projects

--- Cornerstone_Presbyterian_High_School/vite.config.js
+++ Cornerstone_Presbyterian_High_School/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Landings-collection-High_Schools/Cornerstone_Presbyterian_High_School-landing-page/'           //Replace
+  base: '/Landings-collection-High_Schools/Cornerstone_Presbyterian_High_School/'           //Replace
 })
--- Escuela_Secundaria_Tecnica_Mexico/vite.config.js
+++ Escuela_Secundaria_Tecnica_Mexico/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Landings-collection-High_Schools/Escuela_Secundaria_Tecnica_Mexico-landing-page/'           //Replace
+  base: '/Landings-collection-High_Schools/Escuela_Secundaria_Tecnica_Mexico/'           //Replace
 })
